0c99b11602f9b4ca3bfdd7c38abe7ffa969fae51
Jason Gilbert <jasongilbertuk@hotmail.com>
2015-01-17 10:31:09 +0000

    Some client side validation

--- adminAbsenceTypes.php
+++ adminAbsenceTypes.php
@@ -44,14 +44,29 @@ if (isset($_POST["delete"])) {
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="style.css">
 
+       	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      	 	<script>
+  	      function validateCreateFields() 
+  	      {
+  	        	alert("hello");
+  	        	var absenceTypeName = $('#absenceTypeName').val();
+  	        	var usesAnnualLeave = $('#usesAnnualLeave').prop('checked'); 
+  	        	var canBeDenied 	= $('#canBeDenied').prop('checked');
+  	        	
+  	        	alert(absenceTypeName);
+  	        	alert(usesAnnualLeave);
+  	        	alert(canBeDenied);
+  	        	alert("hello");
+  	      }
+    </script>    	
     </head>
  
     <body>
         <?php include 'navbar.php'; ?>
 
-        <form method="post" class="signUp">
+        <form name="createAbsenceType" method="post" onsubmit="return validateCreateFields()">
             <div class="row">
                 <div class="col-md-4 col-md-offset-4 text-center">
                     <h1>Create Absence Type</h1>
@@ -74,7 +89,7 @@ if (isset($_POST["delete"])) {
         </form>
 
        <div class="col-md-8 col-md-offset-2 text-center">
-            <form method="post">
+            <form method="post" >
                 <br/><br/><br/>
                 <h1>Current Absence Types</h1>
                 <table class="table table-bordered table-hover">
@@ -106,5 +121,9 @@ if (isset($_POST["delete"])) {
                 </table>
             </form>
         </div>
+        
+ 
+    }
+}
     </body>
 </html>
